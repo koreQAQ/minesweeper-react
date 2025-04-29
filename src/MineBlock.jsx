f040999f92421f537b9d555123ce383e3a15c370
李树彬 <50092033+koreQAQ@users.noreply.github.com>
2025-04-29 18:11:46 +0800

    Feat add tailwind css

--- src/MineBlock.jsx
+++ src/MineBlock.jsx
@@ -1,13 +1,13 @@
 import './MineBlock.css'
 function MineBlock({ block, handleClick }) {
   function getBlockState() {
-    if (block.revealed) return block.mine ? 'mine' : 'displayed'
+    if (block.revealed) return block.mine ? 'mine' : 'displayed text-yellow-300'
     return block.flagged ? 'flagged' : 'normal'
   }
 
   return (
     <button onClick={handleClick} className={'block' + ' ' + getBlockState()}>
-      {block.revealed ? block.adjacentMines : 'X'}
+      {block.revealed ? block.adjacentMines : ' '}
     </button>
   )
 }
