2a799ffd7a89fa44216a9efbca1657d16e9631e7
李树彬 <50092033+koreQAQ@users.noreply.github.com>
2025-04-29 21:32:56 +0800

    feat: toggle and reset

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import MineBlock from './MineBlock';
 import './Board.css'
 
-
 const directions = [
     [-1, -1],
     [0, -1],
@@ -18,85 +17,103 @@ const directions = [
 
 
 function Board({ width, height }) {
+
+    function generateBlocks() {
+        const newBlocks = Array.from({ length: width },
+            (_, x) => Array.from({ length: height }, (_, y) => {
+                return {
+                    id: x + ',' + y,
+                    x: x,
+                    y: y,
+                    adjacentMines: 0,
+                    revealed: false, mine: Math.random() < 0.1, flagged: false
+                }
+            })
+        )
+        newBlocks.forEach((row) => {
+            row.forEach((block) => {
+                if (!block.mine) {
+                    getSiblings(block, newBlocks).forEach((b) => {
+                        if (b.mine) {
+                            block.adjacentMines += 1
+                        }
+                    })
+                }
+            })
+        })
+        return newBlocks
+    }
+
     function getSiblings(block, blocks) {
         return directions.map(([dx, dy]) => {
             const ox = block.x + dx
             const oy = block.y + dy
-            if (ox < 0 || ox >= 10 || oy < 0 || oy >= 10)
+            if (ox < 0 || ox >= width || oy < 0 || oy >= height)
                 return undefined
             return blocks[ox][oy]
         }).filter(Boolean)
     }
-
-    const generatedBlocks = Array.from({ length: width },
-        (_, x) => Array.from({ length: height }, (_, y) => {
-            return {
-                id: x + ',' + y,
-                x: x,
-                y: y,
-                adjacentMines: 0,
-                revealed: false, mine: Math.random() < 0.1, flagged: false
-            }
-        })
-    )
-
-    generatedBlocks.forEach((row) => {
-        row.forEach((block) => {
-            if (!block.mine) {
-                getSiblings(block, generatedBlocks).forEach((b) => {
-                    if (b.mine) {
-                        block.adjacentMines += 1
-                    }
-                })
-            }
-        })
-    })
-    const [blocks, setBlocks] = useState(generatedBlocks);
-
-    function handleClick(x, y) {
-        const block = blocks[x][y]
+    const [blocks, setBlocks] = useState(generateBlocks());
+    
+    function handleClick(block) {
+        block.revealed = true
         if (block.mine) {
-            console.log(`boom`)
+            console.log('ssss')
+            toggle()
             return
         }
         expandZero(block)
     }
 
     function expandZero(block) {
-        // 返回已经修改过的board
-        // 如果当前这个点不为0
-
-        if (block.adjacentMines == 0) {
-            // 0点要找到周边的0
-            const siblings = getSiblings(block, blocks)
-            console.log(`siblings length: ${siblings.length}`)
-            siblings.forEach((zero) => {
-                if (!zero.revealed) {
-                    zero.revealed = true
-                    expandZero(zero)
-                    const lastBlocks = blocks.slice(0)
-                    setBlocks(lastBlocks)
-                }
+        // 0点要找到周边的0
+        if (!block.adjacentMines){   
+            getSiblings(block, blocks).forEach((zero) => {
+                if (zero.revealed)
+                    return
+                zero.revealed = true
+                expandZero(zero)
             })
-        } else {
-            if (!block.revealed) block.revealed = !block.revealed
-            const lastBlocks = blocks.slice(0)
-            setBlocks(lastBlocks)
         }
+        setBlocks(blocks.slice(0))
+    }
+    
+    const [dev,setDev] = useState(false)
+
+    function toggle() {
+        setDev(!dev)
+        const toggleBlocks = blocks.slice(0)
+        toggleBlocks.forEach((row) => {row.forEach(b => b.revealed = !b.revealed)})
+        setBlocks(toggleBlocks)
     }
 
+    function reset() {
+        setBlocks(generateBlocks())
+    }
 
     const data = blocks.map(row => {
         const values = row.map(block => {
             return (
-                <MineBlock block={block} handleClick={() => handleClick(block.x, block.y)} key={block.id} />
+                <MineBlock block={block} handleClick={() => handleClick(block)} key={block.id} />
             )
         }
         )
         return (<div className="row" >{values}</div>)
     })
+   
     return (
-        <div className='board'>{data}</div>
+        <>
+            <div className='board'>{data}</div>
+            <div className="flex justify-center">
+                <button  className="border-transient w-20" onClick={toggle}>
+                    {dev ? "DEV" : "NORMAL"}
+                </button>
+                <button className="border-transient w-20" onClick={reset}>
+                    RESET
+                </button>
+                
+            </div>
+        </>
     )
 }
 
--- src/MineBlock.jsx
+++ src/MineBlock.jsx
@@ -1,13 +1,24 @@
 import './MineBlock.css'
+const numberColor = [
+  'text-transparent',
+  'text-green-500',
+  'text-blue-500',
+  'text-orange-500',
+  'text-pink-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-gold-500',
+  'text-red-500',
+]
 function MineBlock({ block, handleClick }) {
   function getBlockState() {
-    if (block.revealed) return block.mine ? 'mine' : 'displayed'
-    return block.flagged ? 'flagged' : 'normal'
+    if (block.revealed) return block.mine ? 'bg-red-600/10 text-red-600' : numberColor[block.adjacentMines]
+    return block.flagged ? 'bg-gray-500/10 ' : 'bg-gray-500/10 hover:bg-gray-500/20'
   }
 
   return (
-    <button onClick={handleClick} className={'flex w-10 h-10 m-0.5 justify-center items-center border-1 border-gray-500' + ' ' + getBlockState()}>
-      {block.revealed ? block.adjacentMines : ' '}
+    <button onClick={handleClick} className={"border-1 border-gray-500/10" + " " + getBlockState()}>
+      {block.revealed ? block.mine ? 'X' : block.adjacentMines : ' '}
     </button>
   )
 }
